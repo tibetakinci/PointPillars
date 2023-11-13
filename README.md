6a30d0e5d33fd1aadc5934ab47c0fe7ba9f61f3e
Tibet Akinci <tibetakinci@gmail.com>
2023-11-13 15:42:06 +0300

    README update

--- README.md
+++ README.md
@@ -1,3 +1,94 @@
+# PointPillars: 3D Object Detection from Point Clouds including Wheelchair Users
+
+This repository is forked from PointPillars implementation of [zhulf0804](https://github.com/zhulf0804/PointPillars).
+A slight modification is performed on top of the implementation to add a new class of object, wheelchair users.
+
+## Contents:
+
+- [Setup](#setup)
+  - [Cloning repository](#cloning-repository)
+  - [Conda environment](#setup-conda-environment)
+  - [Compile](#compile)
+  - [Dataset](#dataset)
+    - [Convert dataset](#convert-dataset)
+    - [Prepare dataset](#prepare-dataset)
+- [Training](#training)
+- [Evaluation](#evaluation)
+- [Plotting](#plotting)
+- [Overview of PointPillars](#overview)
+- [Results](#results)
+
+### Setup
+#### Cloning repository
+```
+git clone https://github.com/tibetakinci/PointPillars.git
+cd PointPillars
+```
+
+#### Setup conda environment
+Please visit the [Anaconda install page](https://docs.anaconda.com/free/anaconda/install/index.html) if you do not already have conda installed.
+```
+conda create -n PointPillars
+conda activate PointPillars
+conda install -c anaconda pip
+pip install -r requirements.txt
+```
+
+### Compile
+This block extends pre-implemented C++ functions using [CudaExtension](https://pytorch.org/tutorials/advanced/cpp_extension.html). 
+```
+cd ops
+python setup.py develop
+```
+
+### Dataset
+Format of the dataset should be as follows:
+```
+dataset_name
+|── training
+    ├── label_2 (.txt)
+    └── velodyne (.bin)
+|── testing
+    └── velodyne (.bin)
+```
+
+#### Convert dataset
+To convert dataset into specified format, a [convert_dataset.py](convert_dataset.py) is implemented. 
+This script is utilized to convert point cloud raw data from rosbag to .bin file, label data from .mat file to .txt file.  
+Before running [convert_dataset.py](convert_dataset.py), please export labeled data from Lidar Labeler app in MATLAB as .csv file by running below code box in MATLAB.
+```
+load('your_mat_file.mat')
+tt = gTruth.LabeledData
+writetimetable(tt, 'your_csv_file_name.csv')
+```
+After exporting .csv file, please run the script with correct arguments.
+```
+python convert_dataset.py --bag_data_root your_rosbag_data_root --csv_data_root your_csv_data_root --pc_output_root your_point_cloud_root --label_output_root your_label_data_root
+```
+> Note: *--pc_output_root* and *--label_output_root* parameters are not required. If not specified, the script will create a new folder at parent directory as *velodyne* and *label_2* respectively.
+
+#### Prepare dataset
+Please run the below code to pickle dataset objects for more efficient utilization by the model.
+```
+cd PointPillars
+python pre_process_kitti.py --data_root your_path_to_kitti
+```
+After running [pre_process_kitti.py](pre_process_dataset.py) script, dataset folder should look like this:
+```
+dataset_name
+|── training
+    ├── label_2 (.txt)
+    └── velodyne (.bin)
+|── testing
+    └── velodyne (.bin)
+|── kitti_gt_database (.bin)
+|── kitti_infos_train.pkl
+|── kitti_infos_val.pkl
+|── kitti_infos_test.pkl
+|── kitti_infos_trainval.pkl
+|── kitti_dbinfos_train.pkl
+```
+
 # [PointPillars: Fast Encoders for Object Detection from Point Clouds](https://arxiv.org/abs/1812.05784) 
 
 A Simple PointPillars PyTorch Implenmentation for 3D Lidar(KITTI) Detection. [[Zhihu](https://zhuanlan.zhihu.com/p/521277176)]
--- convert_dataset.py
+++ convert_dataset.py
@@ -8,15 +8,19 @@ import os
 from utils import write_points, write_label
 
 
-def convert_bag2bin(bag_root):
-    output_path = os.path.splitext(bag_root)[0:2]  # TODO
-    bag = rosbag.Bag(bag_root)
+def convert_bag2bin(filename, *args):
+    if args.pc_output_root:
+        output_path = args.pc_output_root
+    else:
+        directory = 'velodyne'
+        output_path = os.path.join(os.path.abspath(filename), directory)
+    bag = rosbag.Bag(filename)
     output_name = 0
     for topic, msg, t in bag.read_messages(topics=['/mec_algo_cloud']):
         pc = point_cloud2.read_points_list(msg, field_names=['x', 'y', 'z', 'intensity'], skip_nans=True)
         pc = np.array(pc, dtype=np.float32)
-        filename = '{:0>6}.bin'.format(output_name)
-        output_file_name = '{0}/{1}'.format(output_path, filename)
+        out_filename = '{:0>6}.bin'.format(output_name)
+        output_file_name = '{0}/{1}'.format(output_path, out_filename)
 
         write_points(pc, output_file_name)
         output_name += 1
@@ -24,28 +28,32 @@ def convert_bag2bin(bag_root):
     bag.close()
 
 
-def convert_csv2txt(csv_root):
-    output_path = os.path.splitext(csv_root)[0:2]  #TODO
-    suffix = os.path.splitext(csv_root)[1]
+def convert_csv2txt(filename, *args):
+    if args.label_output_root:
+        output_path = args.label_output_root
+    else:
+        directory = 'label_2'
+        output_path = os.path.join(os.path.abspath(filename), directory)
+    suffix = os.path.splitext(filename)[1]
     assert suffix == '.csv'
-    with open(csv_root) as tt:
+    with open(filename) as tt:
         csvreader = reader(tt)
         next(csvreader)
 
         output_name = 0
         for row in csvreader:
             if row[1]:
-                filename = '{:0>6}.txt'.format(output_name)
-                output_file_name = '{0}/{1}'.format(output_path, filename)
+                out_filename = '{:0>6}.txt'.format(output_name)
+                output_file_name = '{0}/{1}'.format(output_path, out_filename)
                 result = {
                     'name': 'Wheelchair',
                     'truncated': '0.00',
                     'occluded': '3',
                     'alpha': '-10',
                     'bbox': ' '.join(np.zeros(4, dtype=np.float32)),
-                    'dimensions': np.array(row[2:4]),
-                    'location': np.array(row[4:6]),
-                    'rotation_y': row[7]
+                    'dimensions': np.array(row[4:6]),
+                    'location': np.array(row[1:3]),
+                    'rotation_y': row[9]
                 }
 
                 write_label(result, output_file_name)
@@ -57,9 +65,16 @@ def convert_csv2txt(csv_root):
 def main(args):
     bag_root = args.bag_data_root
     csv_root = args.csv_data_root
+    pc_output_root = args.pc_output_root
+    label_output_root = args.label_output_root
+    print(pc_output_root)
+    pass
 
-    convert_bag2bin(bag_root)
-    convert_csv2txt(csv_root)
+    for filename in os.listdir(bag_root):
+        convert_bag2bin(filename, pc_output_root)
+
+    for filename in os.listdir(csv_root):
+        convert_csv2txt(filename, label_output_root)
 
 
 if __name__ == '__main__':
@@ -68,6 +83,8 @@ if __name__ == '__main__':
                         help='your root for rosbag datas')
     parser.add_argument('--csv_data_root', default='../datasets/ROSBAG/export',
                         help='your root for csv files')
+    parser.add_argument('--pc_output_root', help='output root for point cloud data')
+    parser.add_argument('--label_output_root', help='output root for label data')
     args = parser.parse_args()
 
     main(args)
