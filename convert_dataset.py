79569389831930060d7c2c2d085da1c16492edf9
Tibet Akinci <tibetakinci@gmail.com>
2023-11-24 13:21:00 +0100

    dataset conversion script refactored for multiple file handling

--- convert_dataset.py
+++ convert_dataset.py
@@ -5,13 +5,21 @@ from sensor_msgs import point_cloud2
 import numpy as np
 import os
 
-from utils import write_points, write_label
+from utils import write_points, write_label_no_score
 
 
-def read_bag(bag_root, bag_dir):
+def filter_files(files, suffix):
+    result = []
+    for file in files:
+        if file.endswith(suffix):
+            result.append(file)
+    return result
+
+
+def read_bag(bag_root, bag_dir, pc_topic='/mec_algo_cloud'):
     msgs = []
-    bag = rosbag.Bag(str.join(bag_root, bag_dir))
-    for topic, msg, t in bag.read_messages(topics=['/mec_algo_cloud']):
+    bag = rosbag.Bag(os.path.join(bag_root, bag_dir))
+    for topic, msg, t in bag.read_messages(topics=[pc_topic]):
         msgs.append(msg)
     bag.close()
     return msgs
@@ -21,10 +29,11 @@ def read_csv(csv_root, csv_dir):
     rows = {}
     suffix = os.path.splitext(csv_dir)[1]
     assert suffix == '.csv'
-    with open(str.join(csv_root, csv_dir)) as tt:
+    with open(os.path.join(csv_root, csv_dir)) as tt:
         csvreader = reader(tt)
+        header = next(csvreader)
+        row_id = 0
         for row in csvreader:
-            row_id = 0
             if row[1]:
                 rows[row_id] = row
             row_id += 1
@@ -35,42 +44,60 @@ def read_csv(csv_root, csv_dir):
 def convert_bag2bin(msg, output_root, id):
     pc = point_cloud2.read_points_list(msg, field_names=['x', 'y', 'z', 'intensity'], skip_nans=True)
     pc = np.array(pc, dtype=np.float32)
-    out_filename = str.join(id, '.bin')
+    out_filename = '{:0>6}.bin'.format(id)
     output_file_name = '{0}/{1}'.format(output_root, out_filename)
     write_points(pc, output_file_name)
 
 
 def convert_csv2txt(row, output_root, id):
-    out_filename = str.join(id, '.txt')
+    out_filename = '{:0>6}.txt'.format(id)
     output_file_name = '{0}/{1}'.format(output_root, out_filename)
     result = {
-        'name': 'Wheelchair',
-        'truncated': '0.00',
-        'occluded': '3',
-        'alpha': '-10',
-        'bbox': ' '.join(np.zeros(4, dtype=np.float32)),
-        'dimensions': np.array(row[4:6]),
-        'location': np.array(row[1:3]),
-        'rotation_y': row[9],
-        'score': '0'
+        'name': [],
+        'truncated': [],
+        'occluded': [],
+        'alpha': [],
+        'bbox': [],
+        'dimensions': [],
+        'location': [],
+        'rotation_y': []
     }
+    result['name'].append('Wheelchair')
+    result['truncated'].append('0.00')
+    result['occluded'].append('3')
+    result['bbox'].append(' '.join(['0.00' * 4]))
+    result['alpha'].append('-10')
+    result['dimensions'].append(row[4:6])
+    result['location'].append(row[1:3])
+    result['rotation_y'].append(row[9])
 
-    write_label(result, output_file_name)
+    write_label_no_score(result, output_file_name)
 
 
-def convert_dataset(bag_root, csv_root, pc_output_root, label_output_root):
-    id = 0
-    csv_files = sorted(os.listdir(csv_root))
-    bag_files = sorted(os.listdir(bag_root))
+def convert_dataset(bag_root, csv_root, pc_output_root, label_output_root, start_id):
+    if not os.path.exists(bag_root) or not os.path.exists(csv_root):
+        raise FileNotFoundError
+
+    if pc_output_root is None:
+        pc_output_root = os.path.join(os.path.abspath(bag_root), 'velodyne')
+
+    if label_output_root is None:
+        label_output_root = os.path.join(os.path.abspath(csv_root), 'label')
+
+    os.makedirs(pc_output_root, exist_ok=True)
+    os.makedirs(label_output_root, exist_ok=True)
+    csv_files = filter_files(os.listdir(csv_root), '.csv')
+    bag_files = filter_files(os.listdir(bag_root), '.bag')
     assert len(csv_files) == len(bag_files)
+
     for file_id in range(len(csv_files)):
         assert os.path.splitext(csv_files[file_id])[0] == os.path.splitext(bag_files[file_id])[0]
-        msgs = read_bag(bag_root, bag_files[file_id])
+        msgs = read_bag(bag_root, bag_files[file_id], '')
         rows = read_csv(csv_root, csv_files[file_id])
         for row in rows:
-            convert_bag2bin(msgs[row], pc_output_root, id)
-            convert_csv2txt(rows[row], label_output_root, id)
-            id += 1
+            convert_bag2bin(msgs[row], pc_output_root, start_id)
+            convert_csv2txt(rows[row], label_output_root, start_id)
+            start_id += 1
 
 
 def main(args):
@@ -78,8 +105,9 @@ def main(args):
     csv_root = args.csv_data_root
     pc_output_root = args.pc_output_root
     label_output_root = args.label_output_root
+    start_id = args.start_id
 
-    convert_dataset(bag_root, csv_root, pc_output_root, label_output_root)
+    convert_dataset(bag_root, csv_root, pc_output_root, label_output_root, start_id)
 
 
 if __name__ == '__main__':
@@ -90,6 +118,7 @@ if __name__ == '__main__':
                         help='your root for csv files', required=True)
     parser.add_argument('--pc_output_root', help='output root for point cloud data')
     parser.add_argument('--label_output_root', help='output root for label data')
+    parser.add_argument('--start_id', help='starting id for output files', default=0)
     args = parser.parse_args()
 
     main(args)
