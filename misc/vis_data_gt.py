af8e7e05a0ba80ae28c00d57f0a866d9c3101d13
Tibet Akinci <tibetakinci@gmail.com>
2023-11-30 10:14:28 +0100

    visualise changes

--- misc/vis_data_gt.py
+++ misc/vis_data_gt.py
@@ -18,6 +18,10 @@ def vis_gt(root, id, saved_root):
     lidar_path = os.path.join(root, 'velodyne', f'{id}.bin')
     calib_path = os.path.join(root, 'calib', f'{id}.txt') 
     label_path = os.path.join(root, 'label_2', f'{id}.txt')
+    print(img_path)
+    print(lidar_path)
+    print(calib_path)
+    print(label_path)
 
     img = cv2.imread(img_path)
     img3d = copy.deepcopy(img)
@@ -31,7 +35,8 @@ def vis_gt(root, id, saved_root):
     CLASSES = {
             'Pedestrian': 0, 
             'Cyclist': 1, 
-            'Car': 2
+            'Car': 2,
+            'Wheelchair': 3
             }
 
 
