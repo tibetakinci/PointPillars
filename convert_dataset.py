61c98f0ef01a31db7404372ed3869094c2d650b1
Tibet Akinci <tibetakinci@gmail.com>
2023-11-30 11:30:56 +0100

    dataset conversion maintenance 03

--- convert_dataset.py
+++ convert_dataset.py
@@ -62,13 +62,11 @@ def convert_csv2txt(row, output_root, id):
         'location': [],
         'rotation_y': []
     }
-    print(row[1:3])
-    print(row[4:6])
     result['name'].append('Wheelchair')
     result['truncated'].append('0.00')
     result['occluded'].append('3')
     result['alpha'].append('-10')
-    result['bbox'].append(''.join(['0.00' * 4]))
+    result['bbox'].append(np.array(['0.00' for _ in range(4)]))
     result['dimensions'].append(row[4:7])
     result['location'].append(row[1:4])
     result['rotation_y'].append(row[9])
