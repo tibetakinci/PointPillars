8f6f75dc592347aa0fe4104bdb18c7ca600b0455
tibetakinci <tibetakinci@gmail.com>
2023-12-08 16:36:44 +0000

    refactor on conversion script

--- convert_kitti.py
+++ convert_kitti.py
@@ -6,17 +6,20 @@ from utils import read_calib, read_label, write_label_filtered, bbox_camera2lida
 from convert_custom_dataset import filter_files
 
 
-def convert_to_lidar_coordinate(calib_dict, annotation_dict, out_root, file_name):
-    file_path = '/'.join(out_root, file_name)
+def convert_to_lidar_coordinate(calib_dict, annotation_dict, file_path):
     names = annotation_dict['name']
     dimensions = annotation_dict['dimensions']
     location = annotation_dict['location']
     rotation_y = annotation_dict['rotation_y']
     
     bboxes_camera = np.concatenate([location, dimensions, rotation_y[:, None]], axis=-1) # (N, 7)
+    print('camera')
+    print(bboxes_camera)
     tr_velo_to_cam = calib_dict['Tr_velo_to_cam']
     r0_rect = calib_dict['R0_rect']
     bboxes_lidar = bbox_camera2lidar(bboxes_camera, tr_velo_to_cam, r0_rect)
+    print('lidar')
+    print(bboxes_lidar)
 
     result = {
         'name': [],
@@ -25,10 +28,11 @@ def convert_to_lidar_coordinate(calib_dict, annotation_dict, out_root, file_name
         'rotation_y': []
     }
     result['name'].append(names)
-    result['dimensions'].append(bboxes_lidar[3:6])
-    result['location'].append(bboxes_lidar[0:3])
-    result['rotation_y'].append([bboxes_lidar[6]])
+    result['dimensions'].append(bboxes_lidar[0:, 3:6])
+    result['location'].append(bboxes_lidar[0:, 0:3])
+    result['rotation_y'].append(bboxes_lidar[0:, 6])
 
+    print(result)
     write_label_filtered(result, file_path)
 
 
@@ -37,21 +41,23 @@ def convert_dataset(label_root, calib_root, out_root):
         raise FileNotFoundError
     
     if out_root is None:
-        out_root = os.path.join(os.path.abspath(label_root), 'velodyne')
+        out_root = os.path.join(os.path.abspath(label_root), 'label_2')
 
     os.makedirs(out_root, exist_ok=True)
 
     label_files = sorted(filter_files(os.listdir(label_root), '.txt'))
     calib_files = sorted(filter_files(os.listdir(calib_root), '.txt'))
-    print(label_files)
-    print(calib_files)
-    assert len(label_files) == len(calib_files)
+    #assert len(label_files) == len(calib_files)
 
     for index in range(len(calib_files)):
         assert os.path.splitext(label_files[index])[0] == os.path.splitext(calib_files[index])[0]
-        calib_dict = read_calib(calib_root)
-        annotation_dict = read_label(label_root)
-        convert_to_lidar_coordinate(calib_dict, annotation_dict, out_root, calib_files[index])
+        calib_dir = '/'.join([calib_root, calib_files[index]])
+        label_dir = '/'.join([label_root, label_files[index]])
+        file_path = '/'.join([out_root, label_files[index]])
+        calib_dict = read_calib(calib_dir)
+        annotation_dict = read_label(label_dir)
+        convert_to_lidar_coordinate(calib_dict, annotation_dict, file_path)
+        break
 
 
 def main(args):
