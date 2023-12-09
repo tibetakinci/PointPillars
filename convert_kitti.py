164427ff4b84e880267ba0d43ae0e36c466633ef
tibetakinci <tibetakinci@gmail.com>
2023-12-09 15:40:14 +0000

    conversion maintenance 11

--- convert_kitti.py
+++ convert_kitti.py
@@ -28,9 +28,9 @@ def convert_to_lidar_coordinate(calib_dict, annotation_dict, file_path):
         'rotation_y': []
     }
     result['name'].append(names[0].tolist())
-    result['dimensions'].append(bboxes_lidar[:, 3:6])
-    result['location'].append(bboxes_lidar[:, 0:3])
-    result['rotation_y'].append(bboxes_lidar[:, 6])
+    result['dimensions'].append(bboxes_lidar[:, 3:6].tolist())
+    result['location'].append(bboxes_lidar[:, 0:3].tolist())
+    result['rotation_y'].append(bboxes_lidar[:, 6].tolist())
 
     print(result)
     write_label_filtered(result, file_path)
